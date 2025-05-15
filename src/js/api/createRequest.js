2c7a02859b190001af377e3341a3d7ac360df8f1
Anton Lysenko <antonlysenko161@yandex.ru>
2025-05-15 21:40:03 +0300

    Реализовал функцию createRequest

--- src/js/api/createRequest.js
+++ src/js/api/createRequest.js
@@ -1,3 +1,36 @@
-const createRequest = async (options = {}) => {};
+const createRequest = async (options = {}) => {
+    const {
+        url,
+        method = 'GET',
+        headers = {},
+        body = null,
+    } = options;
+
+    try {
+        const fetchOptions = {
+            method,
+            headers: {
+                'Content-Type': 'application/json', ...headers
+            },
+        }
+
+        if (body) {
+            fetchOptions.body = JSON.stringify(body);
+        }
+
+        const response = await fetch(url, fetchOptions);
+
+        if(!response.ok) {
+            const errorData = await response.json().catch(()=> ({}))
+            throw new Error(`Ошибка: ${response.status} ${response.statusText} \n ${JSON.stringify(errorData)}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Ошибка при выполнении запроса:', error);
+        throw error;
+    }
+
+};
 
 export default createRequest;
